aaab7c1108c9ed6a716b42fd1c11ba529d00d7b4
Oleksandr Komar <35637943+sashakom@users.noreply.github.com>
2020-02-24 22:38:12 +0200

    init: start task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function toReadable (number) {
-  
+  number += '';
+  let result = '';
+  let hundred = 0;
+  let list = {
+              1: 'one',
+              2: 'two',
+              3: 'three',
+              4: 'four',
+              5: 'five',
+              6: 'six',
+              7: 'seven',
+              8: 'eight',
+              9: 'nine',
+              10: 'ten',
+              11: 'eleven',
+              12: 'twelve',
+              13: 'thirteen',
+              14: 'fourteen',
+              15: 'fifteen',
+              16: 'sixteen',
+              17: 'seventeen',
+              18: 'eighteen',
+              19: 'nineteen',
+              20: 'twenty',
+              30: 'thirty',
+              40: 'forty',
+              50: 'fifty',
+              60: 'sixty',
+              70: 'seventy',
+              80: 'eighty',
+              90: 'ninety'
+            };
+
+    if (number.length === 3) {
+      result += list[number[0]] + ' hundred ';
+      result += list[number[1] * 10] + ' ';
+      result += list[number[2]];
+    } else if (number.length === 2) {
+        result += list[number[0] * 10] + ' ';
+        result += list[number[1]];
+    } else if (number.length === 1) {
+        result += list[number[0]];
+    } else {
+        result += 'zero';
+    }
+
+  return result;
 }
